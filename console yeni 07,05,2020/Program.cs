5cf9ed93ba2a4e8d0ff80b5d89060a02a45ff698
Xmuha <Xmuha@DESKTOP-VPOFGR6>
2020-05-07 18:14:04 +0300

    Proje dosyası ekle.

---
+++ console yeni 07,05,2020/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_yeni_07_05_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
